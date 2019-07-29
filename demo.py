49a080c2c0892128865cf14f021df9580cc808c2
Shandira <shandira@Shandiras-MacBook-Air.local>
2019-07-29 10:28:07 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------
+#        Name: <Shandira Soto>
+#    Filename: demo.py
+#        Date: <29 July 2019>
+#
+# Description: sample python file, including a header and a main function
+#               
+# -------------------------------------------------
+
+
+def main():
+    name= Shandira
+    print("Hello, my name is", name)
+
+if __name__=="__main__":
+    main()
